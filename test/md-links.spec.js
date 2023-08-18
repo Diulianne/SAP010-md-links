15fc82a34182654bf8c672b8eb4459fabd6e14f2
Diulianne Oliveira <diulicarine@gmail.com>
2023-08-18 09:22:11 -0400

    melhorei os testes da função mdlinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,13 +1,12 @@
 const mdLinks = require('../index.js');
-
 const path = require('path');
 
 describe('mdLinks', () => {
-  it('should extract links from Markdown content', (done) => {
-    const userFilePath = 'README.md'; // Substitua pelo caminho real do arquivo que o usuário digitar
+  it('Deve extrair links do arquivo Markdown', () => {
+    const userFilePath = 'README.md';
     const filePath = path.resolve(userFilePath); // Resolve o caminho absoluto
 
-    mdLinks(filePath)
+    return mdLinks(filePath)
       .then((links) => {
         expect(links).toHaveLength(links.length); // Verifica se o tamanho do array corresponde ao número de links
         links.forEach((link) => {
@@ -15,27 +14,22 @@ describe('mdLinks', () => {
           expect(link.text).toBeDefined(); // Verifica se o link possui um texto
           expect(link.file).toBeDefined(); // Verifica se o link possui um arquivo
         });
-        done();
       })
       .catch((error) => {
-        done(error); // Notifique o Jest sobre o erro
+        throw error;
       });
   });
 
-  it('should reject if file does not exist', (done) => {
-    const nonExistentFilePath = 'path/to/nonexistent/file.md'; // Substitua pelo caminho real do arquivo inexistente
-
-    mdLinks(nonExistentFilePath)
+  it('Deve rejeitar se o arquivo não existir', () => {
+    const nonExistentFilePath = 'path/to/nonexistent/file.md';
+    return mdLinks(nonExistentFilePath)
       .catch((error) => {
         expect(error).toBeDefined(); // Verifica se um erro foi rejeitado
-        done();
       });
   });
 
-
-  it('should reject if file is not a Markdown file', () => {
+  it('Deve rejeitar se o arquivo não for um arquivo Markdown', () => {
     const nonMarkdownFilePath = path.resolve(__dirname, 'arquivo.txt');
-
     return mdLinks(nonMarkdownFilePath).catch(error => {
       expect(error).toBeDefined(); 
       expect(error.message).toBe('Arquivo não é Markdown'); 
