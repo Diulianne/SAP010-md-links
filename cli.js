c784f32567fcba6d6e54b88759ad97ba3bddc285
Diulianne Oliveira <diulicarine@gmail.com>
2023-08-24 15:15:11 -0400

    fiz pequenas estilizações no terminal

--- cli.js
+++ cli.js
@@ -1,12 +1,12 @@
+const chalk = require('chalk');
 const { mdLinks, validateLink } = require('./index.js');
 
 if (process.argv.length < 3) {
   console.error('Você precisa fornecer o caminho do arquivo como argumento.');
-  process.exit(1); // Encerre o processo com um código de erro
+  process.exit(1); 
 }
 
 const path = process.argv[2];
-// const path =  './README.md'
 const validate = process.argv.includes('--validate');
 const stats = process.argv.includes('--stats');
 
@@ -22,22 +22,22 @@ mdLinks(path)
       const totalLinks = resultLinks.length;
       const uniqueLinks = new Set(resultLinks.map((link) => link.href)).size;
 
-      console.log(`Total: ${totalLinks}`);
-      console.log(`Unique: ${uniqueLinks}`);
+      console.log(`${chalk.bold.cyan('Total: ') + chalk.bold.cyan(totalLinks)} `);
+      console.log(`${chalk.bold.magenta('Unique: ') + chalk.bold.magenta(uniqueLinks)}`);
 
       if (validate) {
         const brokenLinks = resultLinks.filter((link) => link.ok === 'FAIL').length;
-        console.log(`Broken: ${brokenLinks}`);
+        console.log(`${chalk.bold.red('Broken: ') + chalk.bold.red(brokenLinks)} `);
       }
     } else {
       resultLinks.forEach((link) => {
-        console.log(`\nhref: ${link.href}`);
-        console.log(`text: ${link.text}`);
-        console.log(`file: ${link.file}`);
+        console.log(chalk.green(`\nhref: ${link.href}`));
+        console.log(chalk.yellow(`text: ${link.text}`));
+        console.log(chalk.blue(`file: ${link.file}`));
 
         if (validate && link.status !== undefined && link.ok !== undefined) {
-          console.log(`status: ${link.status}`);
-          console.log(`ok: ${link.ok}`);
+          console.log(chalk.magenta(`status: ${link.status}`));
+          console.log(chalk.cyan(`ok: ${link.ok}`));
         }
 
         console.log('------------------------------------------------------------------------');
